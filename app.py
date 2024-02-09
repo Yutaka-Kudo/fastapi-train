1730eab30d6ce79168dbc55b4f185001281274f8
Yutaka-Kudo <68604994+Yutaka-Kudo@users.noreply.github.com>
2024-02-09 08:55:36 +0000

    FastAPIでTodoリストのサンプル作成

--- app.py
+++ app.py
@@ -3,9 +3,69 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
-app = Flask(__name__)
+from fastapi import FastAPI, Depends
+import uvicorn
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+from schemas import PostTodo
+from models import TodoModel
+from settings import SessionLocal
+
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def root():
+    return {"message": "herro"}
+
+# データベースからToDo一覧を取得するAPI
+@app.get("/todo")
+def get_todo(db:Session=Depends(get_db)):
+    # query関数でmodels.pyで定義したモデルを指定し、.all()関数ですべてのレコードを取得
+    return db.query(TodoModel).all()
+
+# ToDoを作成するAPI
+@app.post("/todo")
+def post_todo(
+    todo:PostTodo,
+    db:Session = Depends(get_db)
+):
+    # 受け取ったtitleからモデルを作成
+    db_model = TodoModel(title=todo.title)
+    # データベースに登録（インサート）
+    db.add(db_model)
+    # 変更内容を確定
+    db.commit()
+
+    return {"message":"success add"}
+
+# ToDoを削除するAPI
+@app.delete("/todo/{id}")
+def delete_todo(
+    id:int,
+    db:Session = Depends(get_db)
+):
+    delete_todo = db.query(TodoModel).filter(TodoModel.id==id).one()
+    db.delete(delete_todo)
+    db.commit()
+
+    return {"message":"success delete"}
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
